674707c02a9dab00d0cfafd460f680f258f4e066
KelliOrrela <kelli.orrela@gmail.com>
2013-11-30 10:40:44 +0200

    streamlined code per Pete's suggestions

--- application.js
+++ application.js
@@ -1,79 +1,62 @@
-//Variables
-var randomNumber;
-var guess;
-
-
-//Set up start screen
 $(document).ready(function(){
-   	$(".checkguessbutton, .newgamebutton, .playagainbutton").toggle();
-   	$("#guessinput").hide();
-})
+
+	//Variables
+	var randomNumber;
+	var guess;
+
+	//Hide unneeded visual elements
+	$(".game, .winner").hide();
+  
+});
 
 
-//Set up game
+//Set up to play game or start a new game
 function playTheGame() {
-	$("body").addClass("gamescreen");
-	$(".playthegamebutton, .guessinput, .checkguessbutton, .newgamebutton").toggle();
-	$("#guessinput").show();
-    $(".guess").css({"width": "100%"});
-	document.getElementById("guessinput").value="Type a guess from 1 to 100."; 
 
-	//Generate new random number
+	//Show game screen and hide unneeded visual elements
+	$("body").removeClass("winnerbackground").addClass("gamebackground");
+	$(".start, .winner").hide()
+	$(".game").show();
+
+	//Show instructions in guess field
+	document.getElementById("guess").value="Type a guess from 1 to 100."; 
+
+	//Generate and assign random number
 	randomNumber = (Math.floor(Math.random() * 100) + 1);
 }
-
-
-//Set up game to play again
-function playAgain() {
-	$("body").removeClass("winnerscreen");
-	$("body").addClass("gamescreen");
-	$(".guessinput, .checkguessbutton, .newgamebutton").toggle();
-	$(".playthegamebutton").hide();
-	$("#guessinput, .hot, .cold").show();
-    $(".guess").css({"width": "100%"});
-	document.getElementById("guessinput").value="Type a guess from 1 to 100."; 
-
-	//Generate new random number
-	randomNumber = (Math.floor(Math.random() * 100) + 1);
-}
-
-
-//Generate random number
-randomNumber = (Math.floor(Math.random() * 100) + 1);
 
 
 //Check guess
 function checkGuess() {
 
-	//User inputs guess
-	guess = parseInt(document.getElementById("guessinput").value);
-
+	//Assign guess value
+   	guess = document.getElementById("guess").value;
+	
 	//Check that guess is number
-  	if (isNaN(guess))
-    {
-        document.getElementById("guessinput").value="Not a number! Guess again.";
-   		window.alert("The answer is " + randomNumber + ".");
-    }
+	if (isNaN(guess))
+   		{
+       		document.getElementById("guess").value="Not a number! Guess again.";
+	    }
 
 	//Check if guess is less than random number
-    if (guess < randomNumber)
-    {
-	  	document.getElementById("guessinput").value="Too low! Guess again.";
-    }
+	else if (guess < randomNumber)
+   		{
+  			document.getElementById("guess").value="Too low! Guess again.";
+   		}
 
-//Check if guess is greater than random number
-    else if (guess > randomNumber)
-    {
-		document.getElementById("guessinput").value="Too high! Guess again.";
-    }
+	//Check if guess is greater than random number
+	else if (guess > randomNumber)
+   		{
+			document.getElementById("guess").value="Too high! Guess again.";
+   		}
 
-//Check if guess is equal to random number
-    else if (guess == randomNumber)
-    {
-   	$("guess").css({"width": "18%"});
-  	$("body").addClass("winnerscreen");
-	$(".guessinput, .checkguessbutton, .newgamebutton, .winnertext, .playagainbutton").toggle();
-	$("#guessinput, .playthegamebutton, .hot, .cold").hide();
-    }
+	//Check if guess is equal to random number
+	else if (guess == randomNumber)
+   		{
+   		//Show winner screen and hide unneeded elements
+		$("body").addClass("winnerbackground");
+		$(".game").hide();
+		$(".winner").show();
+		}
 
 }
